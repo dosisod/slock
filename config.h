b7f9e12d35499f2e4692363a7dc3883edc3fce17
dosisod <39638017+dosisod@users.noreply.github.com>
2021-11-17 20:44:06 -0800

    Apply dwmlogo

--- config.h
+++ config.h
@@ -3,10 +3,30 @@ static const char *user  = "noot";
 static const char *group = "noot";
 
 static const char *colorname[NUMCOLS] = {
-	[INIT] =   "#000",     /* after initialization */
+	[BACKGROUND] =   "black",     /* after initialization */
+	[INIT] =   "#2d2d2d",     /* after initialization */
 	[INPUT] =  "#565656",   /* during input */
 	[FAILED] = "#d10407",   /* wrong password */
 };
 
 /* treat a cleared input like a wrong password (color) */
 static const int failonclear = 1;
+
+/* insert grid pattern with scale 1:1, the size can be changed with logosize */
+static const int logosize = 25;
+static const int logow = 12;	/* grid width and height for right center alignment*/
+static const int logoh = 6;
+
+static XRectangle rectangles[9] = {
+	/* x	y	w	h */
+	{ 0,	3,	1,	3 },
+	{ 1,	3,	2,	1 },
+	{ 0,	5,	8,	1 },
+	{ 3,	0,	1,	5 },
+	{ 5,	3,	1,	2 },
+	{ 7,	3,	1,	2 },
+	{ 8,	3,	4,	1 },
+	{ 9,	4,	1,	2 },
+	{ 11,	4,	1,	2 },
+
+};
